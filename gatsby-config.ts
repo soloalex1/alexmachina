1baf5bd2ab0fd11111d3465b170a3209eaba1bd0
Alexandre Machado <amch.smd@gmail.com>
2023-09-07 20:28:59 -0300

    build: remove gatsby gtag plugin

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,24 +1,31 @@
 import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
-  siteMetadata: {
-    title: `Alexandre Machado`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  graphqlTypegen: true,
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-google-gtag", "gatsby-plugin-image", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }]
+	siteMetadata: {
+		title: `Alexandre Machado`,
+		siteUrl: `https://www.yourdomain.tld`,
+	},
+	graphqlTypegen: true,
+	plugins: [
+		"gatsby-plugin-styled-components",
+		"gatsby-plugin-image",
+		{
+			resolve: "gatsby-plugin-manifest",
+			options: {
+				icon: "src/images/icon.png",
+			},
+		},
+		"gatsby-plugin-sharp",
+		"gatsby-transformer-sharp",
+		{
+			resolve: "gatsby-source-filesystem",
+			options: {
+				name: "images",
+				path: "./src/images/",
+			},
+			__key: "images",
+		},
+	],
 };
 
 export default config;

51ac52eda39d1ed6ea828fdfce93c1398e58b543
Alexandre Machado <amch.smd@gmail.com>
2023-11-12 19:36:36 -0300

    refactor: change collapsible to appropriate tags

--- src/components/Collapsible/index.tsx
+++ src/components/Collapsible/index.tsx
@@ -10,44 +10,21 @@ type CollapsibleProps = {
 };
 
 const Collapsible = ({
-	entry: { label, link, description, location, expanded = false },
+	entry: { label, link, description, location },
 }: CollapsibleProps) => {
-	const contentRef = useRef(null);
-	const [open, setOpen] = useState(expanded);
-	const [height, setHeight] = useState(0);
-
-	const toggleOpen = () => {
-		setOpen(!open);
-	};
-
-	useEffect(() => {
-		// @ts-ignore
-		setHeight(open ? contentRef.current.getBoundingClientRect().height : 0);
-	}, [open]);
 
 	return (
-		<section>
-			<S.ItemHeader>
-				<p>{label}</p>
-				<div>
-					{location && link && (
-						<a href={link} target="_blank">
-							{location}
-						</a>
-					)}
-					{description && (
-						<S.ItemButton onClick={toggleOpen}>
-							{open ? <SlArrowUp /> : <SlArrowDown />}
-						</S.ItemButton>
-					)}
-				</div>
-			</S.ItemHeader>
-			<S.CollapsibleContainer height={height}>
-				<div role="content" ref={contentRef} aria-expanded={open}>
-					{description}
-				</div>
-			</S.CollapsibleContainer>
-		</section>
+		<S.Details description={description}>
+			<summary>
+				{label}
+				{location && link && (
+					<a href={link} target="_blank">
+						{location}
+					</a>
+				)}
+			</summary>
+			<p>{description}</p>
+		</S.Details>
 	);
 };
 

3cf89c9bf113fba998d76c7538fcd9d3a02297d9
Alexandre Machado <amch.smd@gmail.com>
2023-11-14 21:41:31 -0300

    style: adjust indent

--- src/components/Collapsible/styles.ts
+++ src/components/Collapsible/styles.ts
@@ -2,7 +2,7 @@ import styled, { css } from "styled-components";
 
 export const Details = styled.details<{ description?: string; }>`
 	${({ description }) => css`
-	padding: 0.5rem;
+	padding: 0.5rem 0;
 
 	summary {
 		list-style-type: ${!description && 'none'};

7e05780f0a5db195c949b605e441a43706e87c8a
Alexandre Machado <amch.smd@gmail.com>
2026-07-21 19:25:26 -0300

    fix typos

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -14,25 +14,18 @@ export const EXPERIENCE_ITEMS: TableList[] = [
 			"Maintained and helped on the evolution of a web platform for a digital bank, refactoring business-focused financial and management features while contributing to new releases and fixes on internal tools.",
 	},
 	{
-		label: "junior front-end developer",
+		label: "junior frontend developer",
 		location: "agenda edu",
 		link: "https://agendaedu.com/",
 		description:
 			"Worked at maintenance and development of Agenda Edu web platform, mostly implementing new features that positively impacted school partners, managers and educators alike. As a part of this process, I've developed performatic graphic interfaces that were easy to use and to learn, acting together with designers and other frontend devs developers to create the company's design system while ensuring consistency and satisfaction through our entire usability experience.",
 	},
 	{
-		label: "front-end developer",
+		label: "frontend developer",
 		location: "statix",
 		link: "https://statix.com.br/",
 		description:
 			"Worked at refining and maintaining legacy web platforms to automate manual workflows of finance and business management, using Angular.js and Java to develop new features and modules. Also developed a few landing pages and digital solutions for external clients using modern tools as React and a few UI-focused libraries such as Material UI, Tailwind and Ant Design.",
-	},
-	{
-		label: "web developer",
-		location: "container digital",
-		link: "http://www.codijr.ufc.br/",
-		description:
-			"Development and maintenance of institutional websites to UFC clients, using Wordpress as the main development and deployment tool.",
 	},
 	{
 		label: "student",
@@ -52,15 +45,15 @@ export const PRESENT_ITEMS: TableList[] = [
 export const DESCRIPTION_ITEMS: TableList[] = [
 	{
 		label:
-			"i model and build simple, accessible and mantainable components and systems",
+			"i model and build simple, accessible and maintainable components and systems",
 	},
 	{
 		label:
-			"i work as a front-end developer who has a strong product and design knowledge",
+			"i work as a frontend developer who has a strong product and design knowledge",
 	},
 	{
 		label:
-			"i have great attention to detail when it comes to implementing layouts, enforcing acessibility and design system rules",
+			"i have great attention to detail when it comes to implementing layouts, enforcing accessibility and design system rules",
 	},
 	{
 		label:
@@ -91,15 +84,5 @@ export const PROJECT_ITEMS: TableList[] = [
 		label: "display",
 		location: "live",
 		link: "https://display-celula.vercel.app/",
-	},
-	{
-		label: "movie list",
-		location: "live",
-		link: "https://lista-filmes-alpha.vercel.app/",
-	},
-	{
-		label: "recursion",
-		location: "live",
-		link: "https://alexmachina.dev",
 	},
 ];
